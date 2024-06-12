8200181c7b680e91f566973fe3590325102d0437
Sayid Rofi <sayidrofi291@gmail.com>
2024-06-12 17:24:08 +0700

    Menambah Halaman Produk 1

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav) {
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
     } else {
         header.classList.remove('navbar-fixed');
@@ -14,23 +14,23 @@ window.onscroll = function() {
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
 
 // Show More Products
-const showButton = document.querySelector('#show');
+const showProductsButton = document.querySelector('#show-products');
 const products = [
     document.querySelector('#product-4'),
     document.querySelector('#product-5'),
     document.querySelector('#product-6')
 ];
 
-showButton.addEventListener('click', function() {
+showProductsButton.addEventListener('click', function() {
     products.forEach(product => {
         product.classList.remove('hidden');
         product.classList.add('flex');
     });
-    showButton.classList.add('hidden');
+    showProductsButton.classList.add('hidden');
 });
